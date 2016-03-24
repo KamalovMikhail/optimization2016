b9e0079ec22d2ba8965af05225847908aafd218f
mikhail <mkamalovv@gmail.com>
2016-03-24 23:55:36 +0300

    1.1 CG Check Tests without disp and trace

--- optim.py
+++ optim.py
@@ -1,7 +1,7 @@
 __author__ = 'mikhail'
 
-
 import numpy as np
+from numpy.linalg import norm
 
 
 A = np.array([[1, 0], [0, 2]])
@@ -16,30 +16,23 @@ def cg(matvec, b, x0, tol=1e-5, max_iter=None, disp=False, trace=False):
     r0 = matvec(xk) - bk
     d0 = - r0
     k = 0
-
+    status = 1
     for i in range(0, len(xk)):
-        print(i)
-        numerator_a = r0.T.dot(r0)
-        denominator_a = d0.T.dot(matvec(d0))
-        alpha_k = np.ndarray(np.divide(numerator_a, denominator_a))
-
-        xk = xk + np.dot(alpha_k, d0)
-        rk = r0 + np.dot(alpha_k, matvec(d0))
-        numerator_b = rk.T.dot(rk)
-        denominator_b = r0.T.dot(r0)
-        betta_k = np.divide(numerator_b, denominator_b)
-
-        d0 = - rk + betta_k.dot(d0)
+        alpha_k = np.dot(r0.T, r0) / np.dot(d0.T, matvec(d0))
+        xk = xk + alpha_k * d0
+        rk = r0 + alpha_k * matvec(d0)
+        betta_k = rk.T.dot(rk) / r0.T.dot(r0)
+        d0 = - rk + betta_k * d0
         k = k + 1
-
         r0 = rk
-
-        if np.norm(matvec(xk) - b) <= tol:
+        if norm(matvec(xk) - b) <= tol:
+            status = 0
             break
-    return xk
+    return xk, status
 
 
 a = cg(matvec, b, x0)
+print(a)
 
 
 
