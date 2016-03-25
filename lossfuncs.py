1538ff3b05a3c5f8fd7463ee53ae3fb6d0693608
mikhail <mkamalovv@gmail.com>
2016-03-25 21:45:40 +0300

    1.3 Matrix function calculation n-dimension with gradient

--- lossfuncs.py
+++ lossfuncs.py
@@ -2,12 +2,12 @@ __author__ = 'mikhail'
 
 import numpy as np
 import scipy.linalg as sp
+import scipy.special as spes
 
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([1, 1, -1, 1])
 reg_coef = 0.5
 w = np.array([0, 0])
-
 
 
 def logistic(w, X, y, reg_coef, hess=False):
@@ -16,9 +16,16 @@ def logistic(w, X, y, reg_coef, hess=False):
     wc = np.copy(w)
     n = len(yc)
 
-    fun_val = 1 / n * np.sum(np.log(1 + np.exp((y * X.T[:]).T[:] * w)), axis=0) + (reg_coef / 2) * np.power(sp.norm(w), 2)
+    # function
+    fun_val = 1 / n * np.sum(np.log(1 + np.exp((-y * X.T[:]).T[:] * w)), axis=0) + (reg_coef / 2) * np.power(sp.norm(w), 2)
+    # gradient
+    spec = spes.expit((-yc * Xk.T[:]).T[:] * wc)
+    sec = ((-yc * Xk.T[:]).T * (np.exp((-yc * Xk.T[:]).T[:] * wc)))
+    grad = 1 / n * np.sum((sec * spec), axis=0) + (reg_coef) * (sp.norm(wc))
 
-    return fun_val
+    return fun_val[0], grad
 
-a = logistic(w, X, y, reg_coef)
-print(a)
+a,b = logistic(w, X, y, reg_coef)
+
+print("!!")
+print(a, b)
