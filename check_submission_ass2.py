1.4 Matrix function calculation n-dimension with correct gradient part of gradient descent (don't work strong Wolf conditions)

check_submission_ass2.py
@@ -5,11 +5,10 @@ from numpy.linalg import norm
 from numpy.testing import assert_equal, assert_almost_equal, assert_array_almost_equal
 import unittest
 from ddt import ddt, data, unpack
-
+from lossfuncs import logistic
 from io import StringIO
 import sys
-
-from optim import cg
+from optim import cg, gd
 
 ############################################################################################################
 # Check if it's Python 3
@@ -47,6 +46,16 @@ X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([1, 1, -1, 1])
 reg_coef = 0.5
 w = np.array([0, 0])
+
+class TestLogistic(unittest.TestCase):
+    def test_default(self):
+        """Check if everything works correctly with default parameters."""
+        f, g = logistic(w, X, y, reg_coef)
+
+        self.assertTrue(isinstance(g, np.ndarray))
+
+        assert_almost_equal(f, 0.693, decimal=2)
+        assert_array_almost_equal(g, [0, -0.25])
 
 
 ############################################################################################################
@@ -96,9 +105,142 @@ class TestCG(unittest.TestCase):
 
         self.assertTrue(isinstance(hist['norm_r'], np.ndarray))
 
+# Simple data
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([1, 1, -1, 1])
+reg_coef = 0.5
+w = np.array([0, 0])
+
+class TestLogistic(unittest.TestCase):
+    def test_default(self):
+        """Check if everything works correctly with default parameters."""
+        f, g = logistic(w, X, y, reg_coef)
+
+        self.assertTrue(isinstance(g, np.ndarray))
+
+        assert_almost_equal(f, 0.693, decimal=2)
+        assert_array_almost_equal(g, [0, -0.25])
+
+    def test_hess(self):
+        """Check that Hessian is returned correctly when `hess=True`."""
+        f, g, H = logistic(w, X, y, reg_coef, hess=True)
+
+        self.assertTrue(isinstance(H, np.ndarray))
+
+        assert_array_almost_equal(H, [[0.625, 0.0625], [0.0625, 0.625]])
+
+
 ############################################################################################################
 ############################################### TestOptim ##################################################
 ############################################################################################################
 
+# Define a simple quadratic function for testing
+A = np.array([[1, 0], [0, 2]])
+b = np.array([1, 6])
+c = 9.5
+x0 = np.array([0, 0])
+
+func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
+func_hess = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b, A))
+# For this func |nabla f(x)| < tol ensures |f(x) - f(x^*)| < tol^2
+
+testing_pairs = (
+    annotated(gd, func),
+    #annotated(ncg, func),
+    #annotated(newton, func_hess),
+)
+
+@ddt
+class TestOptim(unittest.TestCase):
+    @data(*testing_pairs)
+    @unpack
+    def test_default(self, min_method, min_func):
+        """Check if everything works correctly with default parameters."""
+        with Capturing() as output:
+            x_min, f_min, status = min_method(min_func, x0)
+
+        assert_equal(status, 0)
+        self.assertTrue(norm(A.dot(x_min) - b, np.inf) <= 1e-4)
+        self.assertTrue(abs(f_min) <= 1e-8)
+        self.assertTrue(len(output) == 0, 'You should not print anything by default.')
+
+    @data(*testing_pairs)
+    @unpack
+    def test_tol(self, min_method, min_func):
+        """Try high accuracy."""
+        x_min, f_min, status = min_method(min_func, x0, tol=1e-8)
+
+        assert_equal(status, 0)
+        self.assertTrue(norm(A.dot(x_min) - b, np.inf) <= 1e-8)
+        self.assertTrue(abs(f_min) <= 1e-14)
+
+    @data(*testing_pairs)
+    @unpack
+    def test_max_iter(self, min_method, min_func):
+        """Check if argument `max_iter` is supported."""
+        min_method(min_func, x0, max_iter=15)
+
+    @data(*testing_pairs)
+    @unpack
+    def test_max_n_evals(self, min_method, min_func):
+        """Check if the method exceeds the limit on `max_n_evals`."""
+        n_act_evals = 0
+        def min_func_wrapper(x):
+            nonlocal n_act_evals
+            n_act_evals += 1
+            return min_func(x)
+
+        min_method(min_func_wrapper, x0, max_n_evals=1)
+
+        self.assertTrue(n_act_evals <= 1)
+
+    @data(*testing_pairs)
+    @unpack
+    def test_c1(self, min_method, min_func):
+        """Check if argument `c1` is supported."""
+        min_method(min_func, x0, c1=0.2)
+
+    @data(*testing_pairs)
+    @unpack
+    def test_c2(self, min_method, min_func):
+        """Check if argument `c2` is supported."""
+        min_method(min_func, x0, c2=0.1)
+
+    @data(*testing_pairs)
+    @unpack
+    def test_disp(self, min_method, min_func):
+        """Check if something is printed when `disp` is True."""
+        with Capturing() as output:
+            min_method(min_func, x0, disp=True)
+
+        self.assertTrue(len(output) > 0, 'You should print the progress when `disp` is True.')
+
+    @data(*testing_pairs)
+    @unpack
+    def test_trace(self, min_method, min_func):
+        """Check if the history is returned correctly when `trace` is True."""
+        x_min, f_min, status, hist = min_method(min_func, x0, trace=True)
+
+        self.assertTrue(isinstance(hist['f'], np.ndarray))
+        self.assertTrue(isinstance(hist['norm_g'], np.ndarray))
+        self.assertTrue(isinstance(hist['n_evals'], np.ndarray))
+        self.assertTrue(isinstance(hist['elaps_t'], np.ndarray))
+
+        assert_equal(len(hist['norm_g']), len(hist['f']))
+        assert_equal(len(hist['n_evals']), len(hist['f']))
+        assert_equal(len(hist['elaps_t']), len(hist['f']))
+
+        # make sure hist['n_evals'] is a cumulative sum
+        self.assertTrue(np.all(hist['n_evals'] >= 0))
+        self.assertTrue(np.all(hist['n_evals'][1:] - hist['n_evals'][:-1] > 0))
+
+############################################################################################################
+################################################## Main ####################################################
+############################################################################################################
+
+
+
 if __name__ == '__main__':
     unittest.main()
+
+

lossfuncs.py
@@ -10,6 +10,21 @@ reg_coef = 0.5
 w = np.array([0, 0])
 
 
+sigma = spes.expit(-y * X.dot(w))
+
+d = sigma * (1 - sigma)
+
+
+AL = np.zeros((len(d), len(d)), float)
+np.fill_diagonal(AL, d, wrap=True)
+
+print(AL)
+
+print((1 / 4) * np.dot(X.T, AL).dot(X) + 0.5)
+
+
+
+
 def logistic(w, X, y, reg_coef, hess=False):
     yc = np.copy(y)
     Xk = np.copy(X)
@@ -17,15 +32,27 @@ def logistic(w, X, y, reg_coef, hess=False):
     n = len(yc)
 
     # function
-    fun_val = 1 / n * np.sum(np.log(1 + np.exp((-y * X.T[:]).T[:] * w)), axis=0) + (reg_coef / 2) * np.power(sp.norm(w), 2)
+    fun_val = 1 / n * np.sum(np.log(1 + np.exp((-y * X.T).T[:] * w)), axis=0) + (reg_coef / 2) * np.power(sp.norm(w), 2)
+
     # gradient
-    spec = spes.expit((-yc * Xk.T[:]).T[:] * wc)
-    sec = ((-yc * Xk.T[:]).T * (np.exp((-yc * Xk.T[:]).T[:] * wc)))
-    grad = 1 / n * np.sum((sec * spec), axis=0) + (reg_coef) * (sp.norm(wc))
+    sigma = spes.expit(-y * X.dot(w))
+    grad = 1 / n * np.sum((-y * X.T * sigma).T, axis=0) + reg_coef * sp.norm(wc)
 
-    return fun_val[0], grad
+    # hessian
+    if hess:
+        d = sigma * (1 - sigma)
+        D = np.zeros((len(d), len(d)), float)
+        np.fill_diagonal(D, d, wrap=True)
+        hessian = (1 / n) * np.dot(Xk.T, AL).dot(X) + reg_coef
 
-a,b = logistic(w, X, y, reg_coef)
+    if hess:
+        return fun_val[0], grad, hessian
+    else:
+        return fun_val[0], grad
 
-print("!!")
+a,b,h = logistic(w, X, y, reg_coef, hess=True)
+
+#print("!!")
 print(a, b)
+
+print(h)

optim.py
@@ -4,6 +4,7 @@ import scipy.linalg as sp
 import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
+from scipy.optimize.linesearch import line_search_wolfe2
 
 ## b = np.array([1, 6])
 ## x0 = np.array([0, 0])
@@ -21,9 +22,9 @@ def cg(matvec, b, x0, tol=1e-5, max_iter=None, disp=False, trace=False):
     n = len(xk) if (max_iter == None) else max_iter
 
     if trace:
-        hist = {'it': np.array([]), 'n_evals': np.array([])}
+        hist = {'it': np.array([]), 'norm_r': np.array([])}
         hist['it'] = np.append(hist['it'], k)
-        hist['n_evals'] = np.append(hist['n_evals'], nev)
+        hist['norm_r'] = np.append(hist['norm_r'], nev)
 
     if disp:
         print("%10s %15s" % ('iter', 'res_rat'))
@@ -43,7 +44,7 @@ def cg(matvec, b, x0, tol=1e-5, max_iter=None, disp=False, trace=False):
 
         if trace:
             hist['it'] = np.append(hist['it'], k)
-            hist['n_evals'] = np.append(hist['n_evals'], nev)
+            hist['norm_r'] = np.append(hist['norm_r'], nev)
 
         if nev <= tol:
             status = 0
@@ -55,6 +56,39 @@ def cg(matvec, b, x0, tol=1e-5, max_iter=None, disp=False, trace=False):
         return xk, status
 
 
+def gd(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.9, disp=False, trace=False):
+    status = 1
+    x = np.copy(x0)
+    f, gradient = func(x)
+    direction = - gradient
+
+    for i in range(max_iter):
+        print(i)
+        alpha = line_search_wolfe2(f=func, myfprime=func, xk=x, pk=direction, c1=c1, c2=c2)
+        x = x - alpha * gradient
+        f, gradient = func(x)
+        direction = - gradient
+        if norm(gradient) < tol:
+            status = 0
+            break
+
+    return x, f, status
+
+
+def newton(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.9,
+disp=False, trace=False):
+
+
+
+##A = np.array([[1, 0], [0, 2]])
+##b = np.array([1, 6])
+##c = 9.5
+##x0 = np.array([0, 0])
+##func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
+
+##set_x, func, status = gd(func,x0)
+
+##print(set_x, func, status)
 
 def draw_plot(k, n, s):
     color = ['b', 'r', 'g']
@@ -71,12 +105,12 @@ def draw_plot(k, n, s):
             A = V.dot(AL).dot(V.T)
             x, r, l = cg(matvec, B, X0, disp=True, trace=True)
 
-            plt.semilogy(l['n_evals'], l['it'], color[c], label = 'CG' ,alpha = 0.8)
+            plt.semilogy(l['norm_r'], l['it'], color[c], label = 'CG' ,alpha = 0.8)
         c = c + 1
     plt.show()
 
 
-draw_plot([4, 14, 20], 10, 4)
+#draw_plot([4, 14, 20], 10, 4)
 
 
 