1.5 Matrix function calculation n-dimension with correct gradient work gradient descent

check_submission_ass2.py
@@ -1,13 +1,14 @@
-__author__ = 'mikhail'
-
 import numpy as np
 from numpy.linalg import norm
 from numpy.testing import assert_equal, assert_almost_equal, assert_array_almost_equal
 import unittest
 from ddt import ddt, data, unpack
-from lossfuncs import logistic
+
 from io import StringIO
 import sys
+
+from lossfuncs import logistic
+
 from optim import cg, gd
 
 ############################################################################################################
@@ -57,6 +58,13 @@ class TestLogistic(unittest.TestCase):
         assert_almost_equal(f, 0.693, decimal=2)
         assert_array_almost_equal(g, [0, -0.25])
 
+    def test_hess(self):
+        """Check that Hessian is returned correctly when `hess=True`."""
+        f, g, H = logistic(w, X, y, reg_coef, hess=True)
+
+        self.assertTrue(isinstance(H, np.ndarray))
+
+        assert_array_almost_equal(H, [[0.625, 0.0625], [0.0625, 0.625]])
 
 ############################################################################################################
 ################################################# TestCG ###################################################
@@ -105,31 +113,6 @@ class TestCG(unittest.TestCase):
 
         self.assertTrue(isinstance(hist['norm_r'], np.ndarray))
 
-# Simple data
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([1, 1, -1, 1])
-reg_coef = 0.5
-w = np.array([0, 0])
-
-class TestLogistic(unittest.TestCase):
-    def test_default(self):
-        """Check if everything works correctly with default parameters."""
-        f, g = logistic(w, X, y, reg_coef)
-
-        self.assertTrue(isinstance(g, np.ndarray))
-
-        assert_almost_equal(f, 0.693, decimal=2)
-        assert_array_almost_equal(g, [0, -0.25])
-
-    def test_hess(self):
-        """Check that Hessian is returned correctly when `hess=True`."""
-        f, g, H = logistic(w, X, y, reg_coef, hess=True)
-
-        self.assertTrue(isinstance(H, np.ndarray))
-
-        assert_array_almost_equal(H, [[0.625, 0.0625], [0.0625, 0.625]])
-
-
 ############################################################################################################
 ############################################### TestOptim ##################################################
 ############################################################################################################
@@ -139,7 +122,6 @@ A = np.array([[1, 0], [0, 2]])
 b = np.array([1, 6])
 c = 9.5
 x0 = np.array([0, 0])
-
 func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
 func_hess = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b, A))
 # For this func |nabla f(x)| < tol ensures |f(x) - f(x^*)| < tol^2
@@ -147,7 +129,7 @@ func_hess = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b, A))
 testing_pairs = (
     annotated(gd, func),
     #annotated(ncg, func),
-    #annotated(newton, func_hess),
+    # annotated(newton, func_hess),
 )
 
 @ddt
@@ -238,9 +220,5 @@ class TestOptim(unittest.TestCase):
 ################################################## Main ####################################################
 ############################################################################################################
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
-

lossfuncs.py
@@ -10,17 +10,6 @@ reg_coef = 0.5
 w = np.array([0, 0])
 
 
-sigma = spes.expit(-y * X.dot(w))
-
-d = sigma * (1 - sigma)
-
-
-AL = np.zeros((len(d), len(d)), float)
-np.fill_diagonal(AL, d, wrap=True)
-
-print(AL)
-
-print((1 / 4) * np.dot(X.T, AL).dot(X) + 0.5)
 
 
 
@@ -36,14 +25,13 @@ def logistic(w, X, y, reg_coef, hess=False):
 
     # gradient
     sigma = spes.expit(-y * X.dot(w))
-    grad = 1 / n * np.sum((-y * X.T * sigma).T, axis=0) + reg_coef * sp.norm(wc)
+    grad = 1 / n * np.sum((-y * X.T * sigma).T, axis=0) + reg_coef * wc
 
     # hessian
     if hess:
         d = sigma * (1 - sigma)
-        D = np.zeros((len(d), len(d)), float)
-        np.fill_diagonal(D, d, wrap=True)
-        hessian = (1 / n) * np.dot(Xk.T, AL).dot(X) + reg_coef
+        D = np.diag(d)
+        hessian = (1 / n) * np.dot(Xk.T, D).dot(X) + np.diag((reg_coef + np.zeros(len(wc)) ))
 
     if hess:
         return fun_val[0], grad, hessian
@@ -53,6 +41,6 @@ def logistic(w, X, y, reg_coef, hess=False):
 a,b,h = logistic(w, X, y, reg_coef, hess=True)
 
 #print("!!")
-print(a, b)
+print(a, b,h)
 
 print(h)

optim.py
@@ -4,10 +4,11 @@ import scipy.linalg as sp
 import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
-from scipy.optimize.linesearch import line_search_wolfe2
+from scipy.optimize.linesearch import line_search_wolfe2,line_search_armijo
 
 ## b = np.array([1, 6])
 ## x0 = np.array([0, 0])
+
 
 
 
@@ -56,39 +57,54 @@ def cg(matvec, b, x0, tol=1e-5, max_iter=None, disp=False, trace=False):
         return xk, status
 
 
-def gd(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.9, disp=False, trace=False):
-    status = 1
-    x = np.copy(x0)
-    f, gradient = func(x)
-    direction = - gradient
 
-    for i in range(max_iter):
-        print(i)
-        alpha = line_search_wolfe2(f=func, myfprime=func, xk=x, pk=direction, c1=c1, c2=c2)
-        x = x - alpha * gradient
-        f, gradient = func(x)
-        direction = - gradient
-        if norm(gradient) < tol:
+def gd(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.1, disp=False, trace=False):
+    status, x = 1, np.copy(x0)
+    f_0 = (lambda x: func(x)[0])
+    f_1 = (lambda x: func(x)[1])
+
+    f, gradient = func(x)
+    n_act_evals = 1
+
+    for k in range(max_iter):
+        if n_act_evals >= max_n_evals:
+            break
+        if norm(gradient, np.inf) < tol:
             status = 0
             break
+        direction = -gradient
+        alpha = line_search_wolfe2(f=f_0, myfprime=f_1, xk=x, pk=direction, c1=c1, c2=c2)
+        if alpha[0] is None:
+            alpha = line_search_armijo(f=f_0, xk=x, pk=direction, gfk=gradient, old_fval=f)
+
+        x = x - alpha[0] * gradient
+
+        f, gradient = func(x)
+        n_act_evals =+ 1
 
     return x, f, status
 
 
-def newton(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.9,
-disp=False, trace=False):
 
 
 
-##A = np.array([[1, 0], [0, 2]])
-##b = np.array([1, 6])
-##c = 9.5
-##x0 = np.array([0, 0])
-##func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
+#def newton(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.9,
+#disp=False, trace=False):
 
-##set_x, func, status = gd(func,x0)
 
-##print(set_x, func, status)
+
+#A = np.array([[1, 0], [0, 2]])
+#b = np.array([1, 6])
+#c = 9.5
+#x0 = np.array([0, 0])
+
+#func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
+
+
+#set_x, func, status = gd(func, x0)
+
+
+#print(set_x, func, status)
 
 def draw_plot(k, n, s):
     color = ['b', 'r', 'g']