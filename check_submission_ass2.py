1.9 Matrix function calculation n-dimension with correct gradient work gradient descent work newton work nonlinear conjugate gradient with trace and hist Optimize lossfuncs

check_submission_ass2.py
@@ -6,7 +6,7 @@ from ddt import ddt, data, unpack
 
 from io import StringIO
 import sys
-
+from special import grad_finite_diff, hess_finite_diff
 from lossfuncs import logistic
 
 from optim import cg, gd, newton, ncg
@@ -65,6 +65,32 @@ class TestLogistic(unittest.TestCase):
         self.assertTrue(isinstance(H, np.ndarray))
 
         assert_array_almost_equal(H, [[0.625, 0.0625], [0.0625, 0.625]])
+
+############################################################################################################
+############################################ TestFiniteDiff ################################################
+############################################################################################################
+
+# Define a simple quadratic function
+A = np.array([[1, 0], [0, 2]])
+b = np.array([1, 6])
+phi = (lambda x: (1/2)*x.dot(A.dot(x)) + b.dot(x))
+
+class TestFiniteDiff(unittest.TestCase):
+    def test_grad_finite_diff(self):
+        """Check the function returns a correct gradient."""
+        g = grad_finite_diff(phi, np.array([0, 0]))
+
+        self.assertTrue(isinstance(g, np.ndarray))
+
+        assert_array_almost_equal(g, b)
+
+    def test_hess_finite_diff(self):
+        """Check the function returns a correct Hessian."""
+        H = hess_finite_diff(phi, np.array([0, 0]))
+
+        self.assertTrue(isinstance(H, np.ndarray))
+
+        assert_array_almost_equal(H, A)
 
 ############################################################################################################
 ################################################# TestCG ###################################################

lossfuncs.py
@@ -3,11 +3,12 @@ __author__ = 'mikhail'
 import numpy as np
 import scipy.linalg as sp
 import scipy.special as spes
+import scipy.sparse as sps
 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-y = np.array([1, 1, -1, 1])
-reg_coef = 0.5
-w = np.array([0, 0])
+#X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+#y = np.array([1, 1, -1, 1])
+#reg_coef = 0.5
+#w = np.array([0, 0])
 
 
 
@@ -15,32 +16,33 @@ w = np.array([0, 0])
 
 
 def logistic(w, X, y, reg_coef, hess=False):
-    yc = np.copy(y)
-    Xk = np.copy(X)
-    wc = np.copy(w)
-    n = len(yc)
+    yc, Xk, wc, n = np.copy(y), np.copy(X), np.copy(w), len(y)
+    c = -yc * Xk.dot(wc)
 
     # function
-    fun_val = 1 / n * np.sum(np.log(1 + np.exp((-y * X.T).T[:] * w)), axis=0) + (reg_coef / 2) * np.power(sp.norm(w), 2)
+    fun_val = 1 / n * np.sum(np.logaddexp(np.zeros(len(c)), c)) + (reg_coef / 2) * np.power(sp.norm(wc), 2)
 
     # gradient
-    sigma = spes.expit(-y * X.dot(w))
-    grad = 1 / n * np.sum((-y * X.T * sigma).T, axis=0) + reg_coef * wc
+    sigma = spes.expit(c)
+    grad = 1 / n * np.sum((-yc * Xk.T * sigma).T, axis=0) + reg_coef * wc
 
     # hessian
     if hess:
-        d = sigma * (1 - sigma)
-        D = np.diag(d)
-        hessian = (1 / n) * np.dot(Xk.T, D).dot(X) + np.diag((reg_coef + np.zeros(len(wc)) ))
+        d = np.multiply(sigma, (np.ones(n) - sigma))
+        D = sps.diags(d, offsets=0)
+        hessian = (1 / n) * np.dot(Xk.T, D.dot(Xk)) + reg_coef * np.diag(np.ones(len(wc)), 0)
 
     if hess:
-        return fun_val[0], grad, hessian
+        return fun_val, grad, hessian
     else:
-        return fun_val[0], grad
+        return fun_val, grad
 
-a,b,h = logistic(w, X, y, reg_coef, hess=True)
 
+#a,b,h = logistic(w, X, y, reg_coef, hess=True)
+
+#print(a)
+# key phrase extraction
 #print("!!")
-print(a, b,h)
+#print(a, b,h)
 
-print(h)
+#print(h)

optim.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
 import time
+import scipy.sparse as sps
 from scipy.optimize.linesearch import line_search_wolfe2,line_search_armijo
 
 ## b = np.array([1, 6])
@@ -247,16 +248,16 @@ def ncg(func, x0, tol=1e-4, max_iter=500, max_n_evals=1000, c1=1e-4, c2=0.1, dis
 
 
 
-A = np.array([[1, 0], [0, 2]])
-b = np.array([1, 6])
-c = 9.5
-x0 = np.array([0, 0])
+#A = np.array([[1, 0], [0, 2]])
+#b = np.array([1, 6])
+#c = 9.5
+#x0 = np.array([0, 0])
 
-func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
-func_hess = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b, A))
+#func = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b))
+#func_hess = (lambda x: ((1/2)*x.dot(A.dot(x)) - b.dot(x) + c, A.dot(x) - b, A))
 
 
-set_x, func, status, hist = gd(func, x0, disp=True, trace=True)
+#set_x, func, status, hist = gd(func, x0, disp=True, trace=True)
 
 
 
@@ -269,21 +270,23 @@ def draw_plot(k, n, s):
 
     c = 0
     for i in k:
-        for sch in range(1, s):
+        for sch in range(0, s):
             B = np.random.randn(n)
             X0 = np.zeros(n)
             V = sp.orth(np.random.randn(n, n))
-            AL = np.zeros((n, n), float)
-            np.fill_diagonal(AL, np.random.uniform(1, i, n), wrap=True)
-            A = V.dot(AL).dot(V.T)
+            AL = sps.diags(np.random.uniform(1, i, n), 0)
+            A = np.dot(V, AL.dot(V.T))
             x, r, l = cg(matvec, B, X0, disp=True, trace=True)
-
-            plt.semilogy(l['norm_r'], l['it'], color[c], label = 'CG' ,alpha = 0.8)
+            nev = norm(matvec(x) - B)
+            plt.semilogy(l['it'], l['norm_r'] - nev, color[c], label='CG '+str(i), alpha=0.7)
+            plt.xlabel('it')
+            plt.ylabel('norm_r')
+            plt.legend(loc='best')
         c = c + 1
     plt.show()
 
 
-#draw_plot([4, 14, 20], 10, 4)
+#draw_plot([10, 1000, 100000], 1000, 4)
 
 
 

special.py
@@ -1,6 +1,38 @@
 __author__ = 'mikhail'
 
 from lossfuncs import logistic
+import numpy as np
+
+A = np.array([[1, 0], [0, 2]])
+b = np.array([1, 6])
+phi = (lambda x: (1/2)*x.dot(A.dot(x)) + b.dot(x))
 
 def grad_finite_diff(func, x, eps=1e-8):
-    func(x + eps * (0 )) - logistic()
+
+    e = np.zeros(len(x))
+    gradient = np.array([])
+    for i in range(len(x)):
+        e[i] = 1
+        gradient = np.append(gradient, ((func(x + eps * e) - func(x)) / eps))
+        e[i] = 0
+    return gradient
+
+
+def hess_finite_diff(func, x, eps=1e-5):
+    e1, e2, hessian = np.zeros(len(x)), np.zeros(len(x)), np.empty((0, len(x)))
+
+    for i in range(len(x)):
+        row = np.array([])
+        for j in range(len(x)):
+            e2[j], e1[i] = 1, 1
+            row = np.append(row, ((func(x + eps * e1 + eps * e2) - func(x + eps * e1) - func(x + eps * e2) + func(x)) / (eps * eps)))
+            e1[i], e2[j] = 0, 0
+        print(row)
+        hessian = np.append(hessian, np.array([row]), axis=0)
+
+    return hessian
+
+
+g = hess_finite_diff(phi, np.array([0, 0]))
+
+print(g)